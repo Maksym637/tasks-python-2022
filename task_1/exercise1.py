ff2bb020af80676c5689d69c206b5822296091ae
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-03-10 15:32:46 +0200

    commit exercise1

---
+++ task_1/exercise1.py
@@ -0,0 +1,17 @@
+from genericpath import exists
+import math as m
+
+def triangle_area(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        p = (a + b + c) / 2
+        s = m.sqrt(p * (p - a) * (p - b) * (p - c))
+        s_precision_2 = "{:.2f}".format(s)
+        return s_precision_2
+    else:
+        return -1
+
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+print(f's = {triangle_area(a, b, c)}')
