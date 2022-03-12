dd35d1779380f1e0a42c58138fc946456af94f0f
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-03-12 23:03:35 +0200

    commit exercise1

--- task_3/exercise1.py
+++ task_3/exercise1.py
@@ -1,22 +1,33 @@
+from abc import ABC, abstractmethod
+
 class Point():
 
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
 
+class Figure(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def is_point_inside(self):
+        pass
+
 class Triangle():
 
-    def __init__(self, point1, point2, point3):
+    def __init__(self, point1: Point, point2: Point, point3: Point) -> None:
         self.point1 = point1
         self.point2 = point2
         self.point3 = point3
 
-    def area(self):
+    def area(self) -> float:
         return abs((self.point1.x * (self.point2.y - self.point3.y) + 
                     self.point2.x * (self.point3.y - self.point1.y) + 
                     self.point3.x * (self.point1.y - self.point2.y)) / 2.0)
     
-    def is_point_inside(self, point):
+    def is_point_inside(self, point: Point) -> bool:
 
         triangle_1 = Triangle(point, self.point2, self.point3)
         triangle_2 = Triangle(self.point1, point, self.point3)
