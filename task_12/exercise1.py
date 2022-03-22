b6cfc6977b1fc966ef60d2fa38b18a38bc05a688
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-03-22 13:00:22 +0200

    commit exercise1

--- task_12/exercise1.py
+++ task_12/exercise1.py
@@ -2,7 +2,7 @@ import math
 
 class Circle():
     Min_R = 0
-
+    
     def __init__(self, r: int=5) -> None:
         self.__r = r
     
@@ -12,7 +12,7 @@ class Circle():
 
     @radius.setter
     def radius(self, r: int) -> None:
-        if r < self.Min_R:
+        if r < 0:
             raise ValueError("Negative radius is not possible")
         self.__r = r
 
@@ -36,24 +36,24 @@ class Circle():
         return Circle(self.__r - other.__r)
 
 class Ring(Circle):
-    def __init__(self, rin: int=2) -> None:
-        self.__rin = rin
+    def __init__(self, inner_radius: int=2) -> None:
+        self.__inner_radius = inner_radius
     
     @property
     def ring(self) -> int:
-        return self.__rin
+        return self.__inner_radius
     
     @ring.setter
-    def ring(self, rin: int) -> None:
-        if rin < self.Min_R or rin > self.radius:
+    def ring(self, inner_radius: int) -> None:
+        if inner_radius < self.Min_R or inner_radius > self.radius:
             raise ValueError("Such ring radius is not possible")
-        self.__rin = rin
+        self.__inner_radius = inner_radius
     
     def __str__(self) -> str:
-        return f"The ring radius is {self.__rin}"
+        return f"The ring radius is {self.__inner_radius}"
     
     def area(self, circle: Circle) -> float:
-        return math.pi * (pow(circle.radius, 2) - pow(self.__rin, 2))
+        return math.pi * (pow(circle.radius, 2) - pow(self.__inner_radius, 2))
     
     def increase(self, n: int) -> int:
-        return n * self.__rin
+        return n * self.__inner_radius
